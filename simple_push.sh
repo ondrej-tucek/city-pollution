ead7ff6dd04c5cb9c7f7e06774abf6e9826b185d
Ondrej Tucek <ondrej.tucek@gmail.com>
2016-08-13 20:52:08 +0200

    update

--- simple_push.sh
+++ simple_push.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 
-echo -e "\e[91mSimple push to git repo."
+echo -e "\e[93mSimple push to git repo."
+echo -e "\e[39m"
 
 git add . 
 git commit -m "update" 
 git push -u origin master
-
-
 
